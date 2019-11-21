5728fec2a4d0222a4dce611c910dc169a3f5b62d
Giovanni Candeo <giovanni.candeo.1@studenti.unipd.it>
2019-11-21 14:10:15 +0100

    improve plot and print amplitudes of carriers

--- candeo_giovanni.m
+++ candeo_giovanni.m
@@ -28,30 +28,17 @@ xlabel('time[s]'); %xlim([0.1 0.2]);
 title('Original audio signal in time');
 subplot(2,1,2) % show frequency content in dB scale
 plot(fy/1e3,20*log10(abs(Y))); grid; 
-xlim([0 Fp/2e3]); %ylim([-150 -20]) 
+xlim([0 Fp/1e3]); ylim([-200 50]) 
 xlabel('frequency [kHz]'); title('original audio signal in frequency')
+hold on;
+%shows Fp/2 
+plot([1,1]*Fp/2e3,ylim,'r--');
+hold off;
+
 
 %PART 3: find the frequencies f1 and f2 of the sinusoidal carriers
 %by inspection on the spectrum
 %f1 = 13000 f2 = 33600
-
-%index corresponding to the frequency
-i_10k = 10000/F;
-i_27k = 27000/F;
-
-%looking f1 btw 10kHz and 27kHz 
-[s1,pos1] = max(abs(Y(i_10k:i_27k)));
-f1=fy(pos1+i_10k);
-disp(['f1: ' num2str(f1)]);
-
-%looking f2 between 27kHz and 48kHZ
-[s2,pos2] = max(abs(Y(i_27k:Ny/2)));
-f2=fy(pos2+i_27k);
-disp(['f2: ' num2str(f2)]);
-
-
-
-%test123=fy(27000/F)
 
 %{
 f1 and f2 are the frequencies of the two sinusoidal carriers, 
@@ -61,4 +48,33 @@ btwn the 2 modulated components, and specifically so that f2 - f1 >= 17kHz),
 and A1 and A2 are the amplitudes of the carriers. 
 f2 >=
 %}
+
+%index corresponding to the frequency
+i_10k = 10000/F;
+i_27k = 27000/F;
+
+%looking f1 btw 10kHz and 27kHz 
+[A1,pos1] = max(abs(Y(i_10k:i_27k)));
+f1=fy(pos1+i_10k);
+disp(['f1: ' num2str(f1)]);
+
+%looking f2 between 27kHz and 48kHZs
+[A2,pos2] = max(abs(Y(i_27k:Ny/2)));
+f2=fy(pos2+i_27k);
+disp(['f2: ' num2str(f2)]);
+
+hold on;
+%shows f1
+plot([1,1]*f1/1e3,ylim,'g--');
+%shows f2
+plot([1,1]*f2/1e3,ylim,'b--');
+hold off;
+%test123=fy(27000/F)
+legend('signal','Fp/2','f1','f2');
+
+%A1,A2 are the amplitudes of the carriers
+disp(['A1: ',num2str(A1)]);
+disp(['A2: ',num2str(A2)]);
+
+
 
